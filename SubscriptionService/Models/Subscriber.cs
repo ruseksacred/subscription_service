0bc53b1d0ea03b51e30467122821b54a8f18813c
Bartek Rustyn <brustyn123@gmail.com>
2018-05-01 22:54:30 +0200

    atribute_for_subscriber

--- SubscriptionService/Models/Subscriber.cs
+++ SubscriptionService/Models/Subscriber.cs
@@ -10,9 +10,17 @@ namespace SubscriptionService.Models
     {
         [ScaffoldColumn(false)]
         public long ID { get; set; }
+        [StringLength(50,ErrorMessage ="Imię może mieć max 50 znaków")]
+        [Required(ErrorMessage ="Imię jest wymagane")]
+        [Display(Name ="Imie")]
         public string Name { get; set; }
+        [StringLength(50, ErrorMessage = "Nazwisko może mieć max 50 znaków")]
+        [Required(ErrorMessage = "Nazwisko jest wymagane")]
+        [Display(Name = "Nazwsko")]
         public string Surname { get; set; }
         public string Email { get; set; }
+        [Display(Name="Data Rejestracji")]
+        [DataType(DataType.DateTime)]
         public DateTime RegistrationDate { get; set; }
     }
 }
