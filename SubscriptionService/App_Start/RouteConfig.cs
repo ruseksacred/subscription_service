57dcd5948c7ab1f7ca02c14ebae105dde74edd87
Bartek Rustyn <brustyn123@gmail.com>
2018-05-04 21:21:50 +0200

    Newsletter-model+atributes

--- SubscriptionService/App_Start/RouteConfig.cs
+++ SubscriptionService/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SubscriptionService
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Subscriber", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SubscriptionService/Models/Newsletter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SubscriptionService.Models
+{
+    public class Newsletter
+    {
+        [Required(ErrorMessage="Temat jest wymagany")]
+        [Display(Name ="Temat")]
+        [StringLength(20,ErrorMessage ="Tekst nie może być dłuższy niż 20 znaków")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage ="Treść jest wymagana")]
+        [Display(Name ="Treść")]
+        [StringLength(200,ErrorMessage ="Treść nie może być dłuższa niż 200 znaków")]
+        public string Content { get; set; }
+    }
+}
